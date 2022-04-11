5ae520dde27a09558049440a7ce0cd003e03bba6
Chizzard74 <halpern74@protonmail.com>
2022-04-10 21:07:09 -0700

    Created price getting function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests as r
 
-def get_card_by_name(name=None)-> str:
-    """Gets the name of a card and searches for it
+def get_raw_data_by_name(name=None)-> str:
+    """Gets the raw data of a card and searches for it
        on scryfall.net. If no name is provided, ask the
        user in the fucntion call. If the card does not 
        exist, ask the user if they would like to re enter
@@ -19,9 +19,9 @@ def get_card_by_name(name=None)-> str:
         if res.status_code == 404:
             replay =  input("Sorry That is not a valid card name,\nDo you want to try again: type 'yes or no?'")
             if 'y' in replay:
-                get_card_by_name(name)
+                get_raw_data_by_name(name)
             else:
-               return "Goodbye" 
+                return            
         else:
             print(res.text)
     else:            
@@ -30,18 +30,30 @@ def get_card_by_name(name=None)-> str:
         if res.status_code == 404:
             replay =  input("Sorry That is not a valid card name,\nDo you want to try again: type 'yes or no?'")
             if 'y' in replay:
-                get_card_by_name()
+                get_raw_data_by_name()
             else:
-               return "Goodbye"
+                return           
         else:
             print(res.text)    
     return res.text
 
+#This function gets specified currency from user
+def specify_currency()->str:
+    """Asks the user for which currency they wish to use.
+
+    Returns:
+        str: Type of currency the user selects. None otherwise
+    """
+    results = ["usd", "usd_foil", "usd_etched", "eur", "eur_foil", "tix"]
+    display_options = '''Select from one of the following currencies:\n"usd", "usd_foil", "usd_etched", "eur", "eur_foil", "tix"'''    
+    print(display_options)
+    currency = str(input("Enter currency: "))
+    if currency.lower() not in results:
+        return None
+    else:
+        return currency
     
-get_card_by_name("The wandering emperor")
-get_card_by_name()
-
-
+#This function gets the card price 
 def get_card_price(name=None)->int:
     """_summary_
 
@@ -51,7 +63,33 @@ def get_card_price(name=None)->int:
     Returns:
         int: _description_
     """
-    pass
+    currency = specify_currency()
+    if name:
+        res = r.get(f"https://api.scryfall.com/cards/named?fuzzy={name}")
+        if res.status_code == 404:
+            replay =  input("Sorry That is not a valid card name,\nDo you want to try again: type 'yes or no?'")
+            if 'y' in replay:
+                get_card_price(name)
+            else:
+               return "Goodbye" 
+        else:
+            json_response = res.json()
+            print(json_response["prices"][currency])
+    else:
+        name = input("Enter a card name: ")
+        res = r.get(f"https://api.scryfall.com/cards/named?fuzzy={name}")
+        if res.status_code == 404:
+            replay =  input("Sorry That is not a valid card name,\nDo you want to try again: type 'yes or no?'")
+            if 'y' in replay:
+                get_card_price()
+            else:
+                return "Goodbye"
+        else:
+            json_response = res.json()
+            print(json_response["prices"][currency])          
+
+
+#get_card_price()
 
 def get_playable_stats(name=None)-> int:
     """_summary_
