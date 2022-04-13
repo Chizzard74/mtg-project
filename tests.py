c7dfb67cd7e6c4198d247505e862da1275b83820
Chizzard74 <halpern74@protonmail.com>
2022-04-12 19:35:51 -0700

    created class name for TestLibrary

--- tests.py
+++ tests.py
@@ -2,21 +2,27 @@ import unittest
 import time
 from main import Library
 
-def test_startup():    
-    print("testing")
-    print("start")
-    print("Completed")
-    
-def test_initialize_library(self):
-    """Check for a Library.
-    """
-    try:
-        Library()
-    except NameError:
-        self.fail("Library Does Not Exist :(")
-    
-if __name__ == '__main__':
-    unittest.main()
+class TestLibrary(unittest.TestCase):
+
+    def test_startup():    
+        print("testing")
+        print("start")
+        print("Completed")
+        
+    def test_initialize_library(self):
+        """Check for a Library.
+        """
+        try:
+            Library()
+        except NameError:
+            self.fail("Library Does Not Exist :(")
+        
+    def test_size(self):
+        """Library should have 
+        """
+
+    if __name__ == '__main__':
+        unittest.main()
    
 #this tests an empty library
 #lib = Library()
