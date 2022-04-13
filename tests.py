89ded4c611bebf6f539ae43be40d6f991d2a62a6
Chizzard74 <halpern74@protonmail.com>
2022-04-12 19:33:23 -0700

    added test for initialize library

--- tests.py
+++ tests.py
@@ -7,9 +7,16 @@ def test_startup():
     print("start")
     print("Completed")
     
+def test_initialize_library(self):
+    """Check for a Library.
+    """
+    try:
+        Library()
+    except NameError:
+        self.fail("Library Does Not Exist :(")
+    
 if __name__ == '__main__':
-    test_startup()    
-    #unittest.main()
+    unittest.main()
    
 #this tests an empty library
 #lib = Library()
